bb1cc564a039ad3e4967ccf519b88a51021a8806
daisuek4c <biotherapy@forest.ocn.ne.jp>
2016-01-06 22:48:51 +0900

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,27 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+#  scope '/hoge' do
+#    get '/fuga', controller: :hoge, action: :fuga
+#  end
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  def namespaced_scope(path, add_namespace: nil, module_name: nil, &block)
+    scope path, &block
+    scope add_namespace, module: module_name, as: module_name  do
+      scope path, &block
+    end
+  end
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  def scope_with_smartphone(path, &block)
+    scope path, &block
+    scope '/sp', module: :smartphone, as: :smartphone  do
+      scope path, &block
+    end
+  end
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  namespaced_scope '/hoge', add_namespace: :sp, module_name: :smartphone do
+    get '/fuga', controller: :hoge, action: :fuga
+  end
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  scope_with_smartphone 'fuga' do
+    resources :piyo
+  end
 end
